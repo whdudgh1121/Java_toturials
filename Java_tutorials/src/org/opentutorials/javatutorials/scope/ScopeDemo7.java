45313be1fe3563bd28e096c9ddd0f0f0cee87109
whdudgh1121 <79205522+whdudgh1121@users.noreply.github.com>
2024-05-01 03:58:08 +0900

    실습 내용 추가

---
+++ Java_tutorials/src/org/opentutorials/javatutorials/scope/ScopeDemo7.java
@@ -0,0 +1,18 @@
+package org.opentutorials.javatutorials.scope;
+ 
+class C {
+    int v = 10;
+ 
+    void m() {
+        System.out.println(v);
+    }
+}
+ 
+public class ScopeDemo7 {
+ 
+    public static void main(String[] args) {
+        C c1 = new C();
+        c1.m();
+    }
+ 
+}
