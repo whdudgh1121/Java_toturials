84332dd3cfb436c8f7472e603c48b7748f44c9f5
whdudgh1121 <79205522+whdudgh1121@users.noreply.github.com>
2024-05-02 04:11:33 +0900

    114강까지

---
+++ Java_tutorials/src/org/opentutorials/javatutorials/library/LibraryDemo1.java
@@ -0,0 +1,15 @@
+//package org.opentutorials.javatutorials.library;
+//public class LibraryDemo1 {
+//    public static void main(String[] args) {
+//        System.out.println(1);
+//    }
+//}
+
+
+package org.opentutorials.javatutorials.library;
+import java.lang.*;
+public class LibraryDemo1 {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
--- Java_tutorials/src/org/opentutorials/javatutorials/scope/ScopeDemo7.java
+++ Java_tutorials/src/org/opentutorials/javatutorials/scope/ScopeDemo7.java
@@ -4,7 +4,9 @@ class C {
     int v = 10;
  
     void m() {
+    	int v = 20;
         System.out.println(v);
+        System.out.println(this.v);
     }
 }
  
